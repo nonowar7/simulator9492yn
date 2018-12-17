24cd2f60c6e2599e8a039ca7b9475d02a4ad2b9a
yuvalmor <45896198+yuvalmor@users.noreply.github.com>
2018-12-17 22:05:30 +0200

    Update Num.cpp

--- Num.cpp
+++ Num.cpp
@@ -1,19 +1,1 @@
-//
-// Created by noam on 12/17/18.
-//
 
-#include "Num.h"
-
-#include <list>
-#include <string>
-
-using namespace std;
-
-
-Num::Num(double number) {
-    this->number = number;
-}
-
-double Num::calculate() {
-    return this->number;
-}
